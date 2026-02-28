f973c512a647edc97910a1a23424e4b40f15a59d
BrandonMDesk <brandonmendez2323@gmail.com>
2026-02-28 13:32:56 -0700

    A2 q1 finished

--- a2/q1/hamming.c
+++ a2/q1/hamming.c
@@ -1,28 +1,19 @@
 #include <stdio.h>
 
-/** compares
-two natural numbers in a particular base and prints out the base-10 number which represents
-the Hamming distance of those two numbers in the particular base (followed by a newline).
-Your program should take three command line arguments, the first represents one base-10
-natural number, the second represents another base-10 natural number, and the third is
-another base-10 natural number greater than one which represents the base to compare the
-previous two numbers in. 
-**/
 
 void hamming(int a, int b, int base) {
 
     int distance = 0;
     
     while (a > 0 || b > 0) {
-        int digitA = a % base;
-        int digitB = b % base;
-        
-        if (digitA != digitB) {
+        int A = a % base;
+        int B = b % base;
+        if(A != B){
             distance++;
         }
-        
-        a /= base;
-        b /= base;
+        a = a / base;
+        b = b / base;
+       
     }
     
     printf("%d\n", distance);
